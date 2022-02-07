23438c2f4616a303c267f90942c55d0596946fc6
Rasmus Åkerlund <rasmus.akerlund@gmail.com>
2022-02-07 09:39:19 +0100

    update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,23 +1,23 @@
-const core = require("@actions/core")
-const github = require("@actions/github")
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
-    try {
-        const issueTitle = core.getInput("issue-title");
-        const jokeBody = core.getInput("joke");
-        const token = core.getInput("repo-token");
+  try {
+    const issueTitle = core.getInput("issue-title");
+    const jokeBody = core.getInput("joke");
+    const token = core.getInput("repo-token");
 
-        const octokit = new github.getOctokit(token);
+    const octokit = new github.getOctokit(token);
 
-        const newIssue = await octokit.issues.create({
-            repo: github.context.repo.repo,
-            owner: github.context.repo.owner,
-            title: issueTitle,
-            body: jokeBody
-        });
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+    const newIssue = await octokit.issues.create({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-run()
+run();
